84cc9dd40819dfa275460a895b74cd4669f801e6
Harsh Rathore <rathorehrharsh5@gmail.com>
2026-07-23 13:40:26 +0530

    Performing @PostMapping, @DeleteMapping and @PutMapping

--- bootrestbook/src/main/java/com/api/book/controller/BookController.java
+++ bootrestbook/src/main/java/com/api/book/controller/BookController.java
@@ -3,8 +3,12 @@ package com.api.book.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.book.entities.Book;
@@ -12,17 +16,40 @@ import com.api.book.service.BookService;
 
 @RestController
 public class BookController {
-	
+
 	@Autowired
 	private BookService bookservice;
-	
+
 	@GetMapping("/book")
 	public List<Book> getBooks() {
 		return this.bookservice.getAllBook();
 	}
-	
+
 	@GetMapping("/book/{id}")
 	public Book getBook(@PathVariable("id") int id) {
 		return this.bookservice.getBook(id);
+	}
+
+	@PostMapping("/book")
+	public Book addBook(@RequestBody Book book) {
+		Book b = this.bookservice.addBook(book);
+		System.out.println(b);
+		return b;
+	}
+
+	@DeleteMapping("/book/{id}")
+	public boolean deleteBook(@PathVariable("id") int identity) {
+		return this.bookservice.deleteBook(identity);
+	}
+
+	@DeleteMapping("/book")
+	public boolean deleteBooks() {
+		return this.bookservice.deleteAllBook();
+	}
+
+	@PutMapping("/book/{id}")
+	public Book updationBook(@RequestBody Book book,@PathVariable("id") int id) {
+		this.bookservice.updateBook(book,id);
+		return book;
 	}
 }
--- bootrestbook/src/main/java/com/api/book/service/BookService.java
+++ bootrestbook/src/main/java/com/api/book/service/BookService.java
@@ -2,6 +2,7 @@ package com.api.book.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -18,14 +19,54 @@ public class BookService {
 		list.add(new Book(3, "Python", 789));
 	}
 
+//	Get All Book
 	public List<Book> getAllBook() {
 		return list;
 	}
 
+//	Get Specific Book
 	public Book getBook(int id) {
-	
 		Book book = list.stream().filter(b -> b.getId() == id).findFirst().get();
 		return book;
 
 	}
+
+//	Create Book
+	public Book addBook(Book book) {
+		list.add(book);
+		return book;
+	}
+
+//	Delete Specific
+	public boolean deleteBook(int id) {
+		Book book = list.stream().filter(b -> b.getId() == id).findFirst().orElse(null);
+		if (book == null) {
+			return false;
+		}
+		return list.remove(book);
+	}
+
+//	Delete All Book
+	public boolean deleteAllBook() {
+		if (list.isEmpty()) {
+			return false;
+		} else {
+			list.clear();
+			return true;
+		}
+	}
+	 
+//	Update Book
+	public void updateBook(Book book,int id) {
+		list = list.stream().map(b->{
+			
+			if(b.getId() == id) {
+				b.setName(book.getName());
+				b.setPrice(book.getPrice());
+			}
+			return b;
+		}).collect(Collectors.toList());
+
+	}
+
 }
