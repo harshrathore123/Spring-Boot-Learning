a6c94338fd39c43a68c207f80be61783993265a7
Harsh Rathore <rathorehrharsh5@gmail.com>
2026-08-21 22:51:55 +0530

    Today 21 August completeing validation or showing message on ui for Contact Page add contact page

--- smartcontactmanager/src/main/java/com/contact/controller/CustomerController.java
+++ smartcontactmanager/src/main/java/com/contact/controller/CustomerController.java
@@ -1,17 +1,105 @@
-	package com.contact.controller;
+package com.contact.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.contact.dao.UserRepository;
+import com.contact.entities.Contact;
+import com.contact.entities.User;
+import com.contact.helper.Message;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@GetMapping("/data")
-	public String customer_dashboard(Model model) {
+	public String customer_dashboard(Model model, Principal principal) {
+
+		// Getting Username
+		String username = principal.getName();
+		System.out.println("Username" + username);
+
+		// Getting UserDetail
+		User user = userRepository.getUserByEmail(username);
+		System.out.println("User: " + user);
+
+		model.addAttribute("user", user);
 		model.addAttribute("title", "Customer Dashboard - Smart Contact Manager");
-		return "customer_dashboard";
+		return "customer/customer_dashboard";
+	}
+
+	// Open Add Contact Form
+	@GetMapping("/addContact")
+	public String addContact(Model model, Principal principal) {
+
+		// Getting Username
+		String username = principal.getName();
+		System.out.println("Username" + username);
+
+		// Getting UserDetail
+		User user = userRepository.getUserByEmail(username);
+		System.out.println("User: " + user);
+
+		model.addAttribute("user", user);
+
+		model.addAttribute("contact", new Contact());
+		model.addAttribute("title", "Customer Add Contact - Smart Contact Manager");
+		return "customer/customer_add_contact";
+	}
+
+	// Processing Add Contact Form
+	@PostMapping("/process-contact")
+	public String processContact(@Valid @ModelAttribute Contact contact, BindingResult result, Model model,
+			Principal principal, RedirectAttributes redirectAttribute) {
+
+		try {
+			if (result.hasErrors()) {
+				User user = userRepository.getUserByEmail(principal.getName());
+				model.addAttribute("user", user);
+				model.addAttribute("title", "Customer Add Contact - Smart Contact Manager");
+				return "customer/customer_add_contact";
+			}
+
+			// Getting Username
+			String username = principal.getName();
+			System.out.println("Username" + username);
+
+			User usern = this.userRepository.getUserByEmail(username);
+
+			usern.getContact().add(contact);
+
+			this.userRepository.save(usern);
+
+			// Getting UserDetail
+			model.addAttribute("user", usern);
+
+			System.out.println(contact);
+			System.out.println(contact.getNickName());
+			redirectAttribute.addFlashAttribute("message", new Message("Successfully Registered !!", "alert-success"));
+			return "redirect:/customer/customer_add_contact";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			User user = userRepository.getUserByEmail(principal.getName());
+			model.addAttribute("user", user);
+			model.addAttribute("title", "Customer Add Contact - Smart Contact Manager");
+			redirectAttribute.addFlashAttribute("message",
+					new Message("Something went wrong !! " + e.getMessage(), "alert-danger"));
+			return "customer/customer_add_contact";
+		}
 	}
 }
--- smartcontactmanager/src/main/java/com/contact/entities/Contact.java
+++ smartcontactmanager/src/main/java/com/contact/entities/Contact.java
@@ -7,74 +7,115 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name="Contact_Detail")
+@Table(name = "Contact_Detail")
 public class Contact {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int cid;
+
+	@NotBlank(message = "Name is Mendatory!")
 	private String name;
+
 	private String nickName;
-	
-	@Column(nullable=false)
+
+	@NotBlank(message = "Work is Mendatory!")
+	private String work;
+
+	@Column(nullable = false)
+	@Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Email should be in a format")
+	@NotBlank(message = "Email is Mendatory!")
 	private String email;
-	private String imageUrl;
-	
-	@Column(length=1000)
+	private String image;
+
+	@Size(min = 20, max = 100, message = "Description should be between 20 to 100")
 	private String description;
+
+	@NotBlank(message = "Phone number is Mendatory!")
 	private String phoneNumber;
-	
+
 	@ManyToOne
 	private User user;
-	
+
 	public Contact() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public int getCid() {
 		return cid;
 	}
+
 	public void setCid(int cid) {
 		this.cid = cid;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getNickName() {
 		return nickName;
 	}
+
 	public void setNickName(String nickName) {
 		this.nickName = nickName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getImageUrl() {
-		return imageUrl;
+
+	public String getImage() {
+		return image;
 	}
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
+
+	public void setImage(String image) {
+		this.image = image;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	
-	
+
+	public String getWork() {
+		return work;
+	}
+
+	public void setWork(String work) {
+		this.work = work;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [cid=" + cid + ", name=" + name + ", nickName=" + nickName + ", work=" + work + ", email="
+				+ email + ", image=" + image + ", description=" + description + ", phoneNumber=" + phoneNumber
+				+ ", user=" + user + "]";
+	}
+
 }
